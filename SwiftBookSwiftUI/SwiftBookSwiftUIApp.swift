335c939e57a84c2e1889b428d0548bfc6e9b509a
Aigerim Almazbekova <aigerimalmazbekovna1416@gmail.com>
2024-10-10 14:06:34 +0500

    Initial Commit

---
+++ SwiftBookSwiftUI/SwiftBookSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftBookSwiftUIApp.swift
+//  SwiftBookSwiftUI
+//
+//  Created by Айгерим on 10.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftBookSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
